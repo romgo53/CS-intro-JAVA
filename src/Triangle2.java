7ea1c0914c34d0629db2379c758a2131f55d104a
romgo53 <romgo53@gmail.com>
2022-10-23 16:41:07 +0300

    fixed the package issue

---
+++ src/Triangle2.java
@@ -0,0 +1,5 @@
+public class Triangle2 {
+    public static void main(String[] args) {
+        System.out.println("This program calculates the area and the perimeter of a given triangle.");
+    }
+}
