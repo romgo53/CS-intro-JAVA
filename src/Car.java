65b08bbadacf8debb785df980c525ae924bf048e
romgo53 <romgo53@gmail.com>
2022-10-30 19:45:36 +0200

    minor changes

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,9 @@
+/**
+ * Represents a car in a car fleet
+ * @author Rom Amikam Waidergoren
+ * @version 30.10.2022
+ */
+
 public class Car {
     private int _id;
     private char _type;
@@ -8,6 +14,13 @@ public class Car {
     private final String AUTOMATIC_GEAR = "auto";
     private final String MANUAL_GEAR = "manual";
 
+    /**
+     * Initialize the car object with the given parameters.
+     * @param id:       The car's id.
+     * @param type:     The car's type [A,B,C,D].
+     * @param brand:    The car's brand.
+     * @param isManual: The car's gear type [true=manual, false=auto].
+     */
     public Car(int id, char type, String brand, boolean isManual) {
         _id = id;
         _type = type;
@@ -21,6 +34,10 @@ public class Car {
         }
     }
 
+    /**
+     * Initialize the car from other Car object.
+     * @param other:       The other Car object.
+     */
     public Car(Car other) {
         _id = other._id;
         _type = other._type;
@@ -28,10 +45,20 @@ public class Car {
         _isManual = other._isManual;
     }
 
+    /**
+     * Validate that the given type is one of the 4 car types (A,B,C,D).
+     * @param type: The car's type.
+     * @return True if the type is valid, false otherwise.
+     */
     private static boolean isValidType(char type) {
         return type == 'A' || type == 'B' || type == 'C' || type == 'D';
     }
 
+    /**
+     * Validate that the given id is a 7-digit number.
+     * @param id: The car's id.
+     * @return True if the id is valid, false otherwise.
+     */
     private static boolean isValidId(int id) {
         return id >= 1000000 && id < 9999999;
     }
@@ -39,49 +66,77 @@ public class Car {
     public int getId() {
         return _id;
     }
+
     public char getType() {
         return _type;
     }
+
     public String getBrand() {
         return _brand;
     }
+
     public boolean isManual() {
         return _isManual;
     }
+
     public void setId(int id) {
         if (isValidId(id)) {
             _id = id;
         }
     }
+
     public void setType(char type) {
         if (isValidType(type)) {
             _type = type;
         }
     }
+
     public void setBrand(String brand) {
         _brand = brand;
     }
+
     public void setManual(boolean isManual) {
         _isManual = isManual;
     }
+
+    /**
+     * Generates a String that represent the Car's attributes.
+     * @return A String that represent the Car.
+     */
     public String toString() {
         return "id:" + _id + " type:" + _type + " brand:" + _brand + " gear:" + (_isManual ? MANUAL_GEAR : AUTOMATIC_GEAR);
     }
+
+    /**
+     * Checks if the given Car object is equal to this Car object.
+     * @param other: The other Car object.
+     * @return True if the given Car object is equal to this Car object, false otherwise.
+     */
     public boolean equals(Car other) {
         return _type == other._type && _brand.equals(other._brand) && _isManual == other._isManual;
     }
+
+    /**
+     * Checks if this Car object is better that the given one.
+     * Compares the types while A is the worst and D is the best.
+     * If the types are equal, it checks if this car is auto and the other is manual.
+     * @param other: The other Car object.
+     * @return True if this Car is better than the other Car object, false otherwise.
+     */
     public boolean better(Car other) {
-         if (_type > other._type)
-             return true;
+        if (_type > other._type)
+            return true;
         return _isManual && !other._isManual;
     }
+
+    /**
+     *Checks if this car is worse than the other Car.
+     * Same as the `better` method but this time it checks if the other car is better.
+     * @param other: The other Car object.
+     * @return True if this Car is better than the other Car object, false otherwise.
+     */
     public boolean worse(Car other) {
         return other.better(this);
-    }
-    public static void main(String[] args) {
-        Car car = new Car(1234567, 'A', "Toyota", false);
-        Car car2 = new Car(1232567, 'A', "Toyota", true);
-        System.out.println(car2.worse(car));
     }
 }
 
